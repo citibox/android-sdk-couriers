9f89e2fd7cc0ef8c95ac949bde06ec2ec4892848
juancarloscarrillo-citibox <juancarloscarrillo@citibox.com>
2023-12-14 15:28:51 +0100

    chore: aesthetic environment selector

--- app/src/main/java/com/citibox/courier/sdk/example/compose/EnvironmentSelector.kt
+++ app/src/main/java/com/citibox/courier/sdk/example/compose/EnvironmentSelector.kt
@@ -2,15 +2,21 @@ package com.citibox.courier.sdk.example.compose
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.outlined.Build
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,6 +24,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import com.citibox.courier.sdk.webview.models.WebAppEnvironment
 
@@ -25,11 +32,12 @@ import com.citibox.courier.sdk.webview.models.WebAppEnvironment
 fun EnvironmentSelector(actual: WebAppEnvironment, onItemClicked: (WebAppEnvironment) -> Unit) {
     var expanded by remember { mutableStateOf(false) }
 
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
     ) {
+        Text(text = "Environment")
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier
@@ -37,8 +45,17 @@ fun EnvironmentSelector(actual: WebAppEnvironment, onItemClicked: (WebAppEnviron
                 .clickable {
                     expanded = true
                 }
+                .padding(vertical = 8.dp, horizontal = 4.dp)
         ) {
-            Text(text = "Environment: ${actual.name}")
+            Row {
+                Icon(
+                    actual.icon(),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.secondary
+                )
+                Spacer(modifier = Modifier.size(8.dp))
+                Text(text = actual.name)
+            }
             Icon(
                 imageVector = Icons.Default.ArrowDropDown,
                 contentDescription = "More"
@@ -46,11 +63,26 @@ fun EnvironmentSelector(actual: WebAppEnvironment, onItemClicked: (WebAppEnviron
         }
         DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
             WebAppEnvironment.entries.forEach {
-                DropdownMenuItem(text = { Text(it.name) }, onClick = {
-                    onItemClicked(it)
-                    expanded = false
-                })
+                DropdownMenuItem(
+                    text = { Text(it.name) },
+                    onClick = {
+                        onItemClicked(it)
+                        expanded = false
+                    },
+                    leadingIcon = {
+                        Icon(
+                            it.icon(),
+                            contentDescription = null
+                        )
+                    }
+                )
             }
         }
     }
+}
+
+private fun WebAppEnvironment.icon(): ImageVector = when (this) {
+    WebAppEnvironment.Production -> Icons.Outlined.CheckCircle
+    WebAppEnvironment.Sandbox -> Icons.Outlined.Build
+    WebAppEnvironment.Test -> Icons.Outlined.Warning
 }
