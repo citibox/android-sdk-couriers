10da69f7721b5ffb73c1070e563e8f19d6b18104
juancarloscarrillo-citibox <juancarloscarrillo@citibox.com>
2024-01-23 11:11:50 +0100

    chore: removed app

--- app/src/main/java/com/citibox/courier/sdk/example/delivery/models/DeliverySideEffect.kt
+++
@@ -1,9 +0,0 @@
-package com.citibox.courier.sdk.example.delivery.models
-
-import com.citibox.courier.sdk.domain.DeliveryParams
-
-sealed class DeliverySideEffect {
-
-    data class Launch(val params: DeliveryParams) : DeliverySideEffect()
-
-}
--- app/src/main/java/com/citibox/courier/sdk/example/retrieval/models/RetrievalSideEffect.kt
+++
@@ -1,9 +0,0 @@
-package com.citibox.courier.sdk.example.retrieval.models
-
-import com.citibox.courier.sdk.domain.RetrievalParams
-
-sealed class RetrievalSideEffect {
-
-    data class Launch(val params: RetrievalParams): RetrievalSideEffect()
-
-}
